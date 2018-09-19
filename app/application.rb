25069773ab7d6ad0e558dd1988d2d886b12042c6
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-19 20:41:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if resp != req.path.match(/items/)
+      resp.write "Route not Found"
+    end
 
   end
 
